6faeee42fac9d1be616dd237dbd6ef3c57bd6262
jerrydngzh <zrd1@sfu.ca>
2023-09-30 17:40:20 -0700

    refactor classes against SocialGaming specification & validate cmake external dependencies build. Authors:zrd1,cga57

--- clientSource/model/include/game.h
+++ clientSource/model/include/game.h
@@ -1,18 +1,26 @@
 #include <cstdlib>
+#include <fstream>
+
 
 enum GameType {
    RockPaperScissors
 };
 
-
-
+// This Game class is roughly meant to represent the confiuration json
+// defined in the project description. Have switched design
+// from inheritance to composition
 class Game {
     
 public:
     // Pure virtual function, making it an abstract method
-    virtual GameType getTypeOfGame() = {
+    GameType getTypeOfGame() = {
         return typeOfGame;
     };
+    
+    // Creates Json File to send to server
+    // PRE: No arguments (uses the variables it already has to build it)
+    // POST: Returns a json file 
+    std::ifstream createJsonFile();
 
     // You can also define other non-pure virtual functions with a default implementation
     virtual std::tuple<int, int> getPlayerRange() {
@@ -28,5 +36,11 @@ private:
     
     // change it from std::string to something else later - avoid stringify
     // configuration contains many items + another map for "setup"
-    std::unordered_map<std::string,std::unordered_map<std::string, std::string>> configuration;
+    std::unordered_map<std::string, std::unordered_map<std::string, std::string>> setupSettings;
+
+    // this is how a unique point can be definded
+    // std::unique_ptr<MyClass> myPtr = std::make_unique<MyClass>(42);
+    // TODO: improve on value type (instead of void*)
+    std::unorded_map<std::string_view, std::unique_ptr<void*>> variables;
+    std::unorded_map<std::string_view, std::unique_ptr<void*>> constants;
 };
--- clientSource/model/include/gameroom.h
+++ clientSource/model/include/gameroom.h
@@ -1,18 +1,24 @@
 #include <string_view>
 #include "user.h"
 
-class GameRoom : public Game{
+/*
+Contains all the information regarding players and pre-game setup
+*/
+class GameRoom{
 public: 
 private:
     const std::string_view gameCode;
 
+    // A GameRoom has-a Game (composition)
+    std::unique_ptr<GameRoom> game;
+
     // this is how a unique point can be definded
     // std::unique_ptr<MyClass> myPtr = std::make_unique<MyClass>(42);
     // TODO: improve on value type (instead of void*)
-    std::unorded_map<std::string, std::unique_ptr<void*>> variables;
+    std::unorded_map<std::string_view, std::unique_ptr<void*>> variables;
 
     // User has fields to determine if is player, audience, or owner
     // maybe explore better way than that in defining it  
-    std::unorded_map<std::string, std::unique_ptr<User>> perPlayer;
-    std::unorded_map<std::string, std::unique_ptr<User>> perAudience;
+    std::unorded_map<std::string_view, std::unique_ptr<User>> perPlayer;
+    std::unorded_map<std::string_view, std::unique_ptr<User>> perAudience;
 }
