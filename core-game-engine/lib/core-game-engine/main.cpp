aefb9a964f9e3c99e72911d4d875a1c56733b439
mkreutz <mkreutz@sfu.ca>
2023-10-17 22:20:06 -0700

    got parsing tree stored in vector, need to cleanup code

--- core-game-engine/lib/core-game-engine/main.cpp
+++ core-game-engine/lib/core-game-engine/main.cpp
@@ -40,72 +40,142 @@ auto recursiveParse(const ts::Node& node, const std::string &fileContents, auto 
     }
 }
 
-// constants {      # VECTOR = value_map
-//   weapons: [     # VECTOR = 
-//     { name: "Rock",     beats: "Scissors" },   # VECTOR of structs
-//     { name: "Paper",    beats: "Rock"     },
-//     { name: "Scissors", beats: "Paper"    },
-//   ]
-// }
 
-void extractMap(const ts::Node& node, const std::string &fileContents, std::vector<auto> &entry){
+struct CustomMapping {
+    int index;
+    std::string_view key;
+    std::string_view value;
+    int parent;
+    std::vector<int> children;
+    std::string_view type;
+};
+
+void extractMap(const ts::Node& node, const std::string &fileContents, std::vector<auto> &entry, CustomMapping &mapping){
     // DEBUG
     std::string_view type = node.getType();
-    std::cout << std::endl << "[DEBUG] " << "Type: " << type << std::endl;
     std::string_view contents = node.getSourceRange(fileContents);
+    std::cout << std::endl;
     std::cout << "[DEBUG] " << contents << std::endl;
+    std::cout << "[DEBUG] type: " << type << std::endl;
     std::cout << "NumChildren:" << node.getNumChildren() << std::endl;
 
-    if(node.getNumChildren() == 0) {
-      if(type == "identifier") {
-        // add to key
-        std::cout << "found identifier" << std::endl;
-        std::string_view key = contents;
-        std::cout << "key = " << key << std::endl;
-      } else if(type == "quoted_string") {
-        // add to value
-        std::cout << "found quoted_string" << std::endl;
-      } else if(type == "{" || type == ":" || type == "}" || type == "[" || type == "]") {
-        std::cout << "N/A" << std::endl;
-      }
-    } else {
-      
-          for(int i = 0; i < node.getNumChildren(); i++) {
-
-            // 1. number; 2. boolean; 3. quoted_string; 4. number_range; 5. identifiers; 6. expression_list; 7. list_literal
-            // 8. map_entry; 9. value_map; 10. qualified_identifier; 11. player_set; 12. body; 13. comment
-
-            // TODO, need expression
-            if(type == "value_map") {
-              // create new map (will be list of maps)
-              // std::cout << "found value_map" << std::endl;
-              // std::map<std::string_view, std::any> tempMap;
-
-              extractMap(node.getChild(i), fileContents, entry);
-            } else if(type == "map_entry") {
-              // TBD
-              // std::cout << "found map_entry" << std::endl;
-
-              extractMap(node.getChild(i), fileContents, entry);
-
-              // entry.insert(std::make_pair(localKey, localValue));
-            } else if(type == "list_literal") {
-              // for loop through children
-              // std::cout << "found list_literal" << std::endl;
-              // extractMap(node.getChild(i), fileContents, entry, key, value);
-            } else if(type == "expression") {
-              // for loop through children
-              // std::cout << "found expression" << std::endl;
-              // extractMap(node.getChild(i), fileContents, entry, key, value);
-            } else if(type == "expression_list") {
-              // for loop through children
-              // std::cout << "found expression_list" << std::endl;
-              // extractMap(node.getChild(i), fileContents, entry, key, value);
-            } else {
-              std::cout << "N/A" << std::endl;
-            }
+    //         // 1. number; 2. boolean; 3. quoted_string; 4. number_range; 5. identifiers; 6. expression_list; 7. list_literal
+    //         // 8. map_entry; 9. value_map; 10. qualified_identifier; 11. player_set; 12. body; 13. comment
+    if(type == "identifier") {
+      mapping.key = contents;
+    } else if (type == "number") {
+      // TODO
+    } else if (type == "boolean") {
+      // TODO
+    } else if (type == "quoted_string") {
+      mapping.value = contents;
+      mapping.index = entry.size();
+      mapping.type = type;
+      entry.at(mapping.parent).children.push_back(mapping.index);
+      entry.push_back(mapping);
+      mapping.key = "";
+      mapping.value = "";
+    } else if (type == "number_range") {
+      // TODO
+    } else if (type == "expression") {
+      extractMap(node.getChild(0), fileContents, entry, mapping);
+    } else if (type == "expression_list") {
+      int parent = mapping.parent;
+      for(int i = 0; i < node.getNumChildren(); i++){
+        if(node.getChild(i).getType() != ",") {
+          CustomMapping tempMapping{};
+          tempMapping.index = entry.size();
+          tempMapping.type = type;
+          tempMapping.parent = mapping.parent;
+          if(entry.size() > 0) {
+            entry.at(mapping.parent).children.push_back(tempMapping.index);
           }
+          entry.push_back(tempMapping);
+          mapping.parent = tempMapping.index;
+          extractMap(node.getChild(i), fileContents, entry, mapping);
+        }
+        mapping.parent = parent;
+      }
+    } else if (type == "list_literal") {
+      CustomMapping tempMapping{};
+      tempMapping.key = mapping.key;
+      tempMapping.index = entry.size();
+      tempMapping.type = type;
+      tempMapping.parent = mapping.parent;
+      if(entry.size() > 0) {
+        entry.at(mapping.parent).children.push_back(tempMapping.index);
+      }
+      entry.push_back(tempMapping);
+      mapping.parent = tempMapping.index;
+      for(int i = 0; i < node.getNumChildren(); i++){
+        extractMap(node.getChild(i), fileContents, entry, mapping);
+      }
+    } else if (type == "map_entry") {
+      for(int i = 0; i < node.getNumChildren(); i++){
+        extractMap(node.getChild(i), fileContents, entry, mapping);
+      }
+    } else if (type == "value_map") {
+      for(int i = 0; i < node.getNumChildren(); i++){
+        extractMap(node.getChild(i), fileContents, entry, mapping);
+      }
+    } else if (type == "qualified_identifier") {
+      // TODO
+    } else if (type == "player_set") {
+      // TODO
+    } else if (type == "body") {
+      // TODO
+    } else if (type == "comment") {
+      // TODO
     }
+
+
+    //   if(type == "{" || type == "[" || type == ",") {  
+    //     return;
+    //   } else if(type == "identifier") {
+    //     // std::cout << "found identifier" << std::endl;
+    //     std::string_view key = contents;
+    //     // std::cout << "key = " << key << std::endl;
+    //     mapping.index = entry.size();
+    //     mapping.type = "identifier";
+    //     mapping.key = contents;
+    //     return;
+    //   } else if(!mapping.key.empty() && type == "quoted_string"){
+    //   // } else if(type == "quoted_string") {
+    //     // std::cout << "found quoted_string" << std::endl;
+    //     std::string_view value = contents;
+    //     // std::cout << "value = " << value << std::endl;
+    //     mapping.index = entry.size();
+    //     mapping.type = type;
+    //     mapping.value = value;
+    //     entry.push_back(mapping);
+    //     mapping.key = "";
+    //     mapping.value = "";
+    //     mapping.type = "";
+    //     return;
+    // } else {
+    //       for(int i = 0; i < node.getNumChildren(); i++) {
+    //         // 1. number; 2. boolean; 3. quoted_string; 4. number_range; 5. identifiers; 6. expression_list; 7. list_literal
+    //         // 8. map_entry; 9. value_map; 10. qualified_identifier; 11. player_set; 12. body; 13. comment
+    //         if(type == "value_map") {
+    //           // std::cout << "found value_map" << std::endl;
+    //           extractMap(node.getChild(i), fileContents, entry, mapping);
+    //         } else if(type == "map_entry") {
+    //           // std::cout << "found map_entry" << std::endl;
+    //           extractMap(node.getChild(i), fileContents, entry, mapping);
+    //         } else if(type == "list_literal") {
+    //           // std::cout << "found list_literal" << std::endl;
+    //           extractMap(node.getChild(i), fileContents, entry, mapping);
+    //         } else if(type == "expression") {
+    //           // std::cout << "found expression" << std::endl;
+    //           extractMap(node.getChild(i), fileContents, entry, mapping);
+    //         } else if(type == "expression_list") {
+    //           // std::cout << "found expression_list" << std::endl;
+    //           extractMap(node.getChild(i), fileContents, entry, mapping);
+    //         } else {
+    //           std::cout << "N/A" << std::endl;
+    //         }
+    //       }
+    // }
 }
 
 
@@ -192,16 +262,29 @@ int main(int argc, char* argv[]) {
   printVector(constantsParsedResults);
 
   std::cout << "******************** MAP EXTRACTOR *****************" << std::endl;
-  std::vector<std::string_view> tempVector = {};
+  std::vector<CustomMapping> tempVector = {};
+  CustomMapping tempMapping = {};
   
-  extractMap(constants.getChild(1), fileContents, tempVector);
+  extractMap(constants.getChild(1), fileContents, tempVector, tempMapping);
+  std::cout << tempVector[0].key << std::endl;
+  std::cout << tempVector.size() << std::endl;
+
+  for(const CustomMapping& entry : tempVector){
+    std::cout << "Index   : " << entry.index << std::endl;
+    std::cout << "Key     : " << entry.key << std::endl;
+    std::cout << "Value   : " << entry.value << std::endl;
+    std::cout << "Type    : " << entry.type << std::endl;
+    std::cout << "Parent  : " << entry.parent << std::endl;
+    std::cout << "Children: [";
+    for(const int &child : entry.children){
+      std::cout << child << ", ";
+    }
+    std::cout << "]" << std::endl << std::endl;
+  }
 
 
-  // auto newMap = convertStringToMap(constantsParsedResults, 5);
-
-  // std::cout << newMap.size() << std::endl;
-  // std::cout << newMap.at("name") << std::endl;
-  // std::cout << newMap.at("beats") << std::endl;
+  std::cout << tempMapping.key << std::endl;
+  std::cout << tempMapping.value << std::endl;
 
   //rulesLibrary.runGame()?
 }
