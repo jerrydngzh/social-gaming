e1735ace80bdec526650a4f22a5a822bbba152b9
SFUMisha <mea28@sfu.ca>
2023-10-18 14:01:09 -0700

    initial file setup

---
+++ core-game-engine/lib/core-game-engine/include/interpreter.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <string>
+#include <cpp-tree-sitter.h>
+#include <map>
+#include <string_view>
+#include "game.h"
+
+
+// Handles the execution of a game
+class Interpreter {
+private:
+    Game game;
+    ts::Tree tree;
+
+public:
+    Interpreter(Game game, ts::Tree tree);
+    ~Interpreter();
+    void recurse(ts::Node n);
+    const static std::map<ts::Symbol, Action*> actions;
+    static ts::Symbol toSymbol(const std::string_view& symbolName);
+
+    class Action {
+    public:
+        virtual void execute(ts::Node node) = 0;
+        virtual ~Action() {}
+    };
+};
