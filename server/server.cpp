0333cc6a6707516899a1188ab9f6fddb25194c72
cga57 <cga57@sfu.ca>
2023-10-29 22:52:29 -0700

    Implemented partial server functionality, changed parts of design. Authors: cga57,kka116

--- server/server.cpp
+++ server/server.cpp
@@ -1,22 +1,54 @@
 #pragma once
 #include <string>
 #include <vector>
+#include <unordered_map>
+#include "server.h"
+
 
 
 // int clientAddress - > clientID
+Server::Server(){}
 
-class Server {
-public:
-    Server();
+void Server::addNewClient(int clientId){
+    Server::clientIDToMessageMap.insert(std::make_pair(1, ""));
+}
+void Server::removeClient(int clientId){
+    Server::clientIDToMessageMap.insert(std::make_pair(1, ""));
+}
+// helper function
+void Server::changeClientMessage(int clientId, std::string newMessage){
+    clientIDToMessageMap[clientId] = std::string(newMessage);
+}
 
-    std::vector<std::string> recieveMessageFromAllClients(std::vector<int> clientIDs);
+std::string Server::requestMessageFromClient(int clientID){
+   
+}
 
-    std::string concatenateAllMessages(std::vector<std::string> clientMessages);
+// do processing of the message recieved
+void Server::recieveMessageFromClient(int clientID, std::string message){
+    // we need to add a message that is sent to the server upon client construction through the 
+    // mailman api
+    if (message == "new client"){
+        this->addNewClient(clientID);
+    } else if (message == "remove client"){
+        this->removeClient(clientID);
+    }
+    this->changeClientMessage(clientID,message);
+    // Further business processing of messages will be here
+    // TODO: We will need to know what type of messages are there and figure out
+    // which ones to send back
+}
+
+
+std::string concatenateAllMessages(){
+
+
+}  
     
-    void broadcastResultsToAllClients(std::string results);
+void broadcastResultsToAllClients(std::string results){
+    
+}
 
-private:
-    std::vector<int> clientIDs; 
-    std::vector<std::string> clientMessages;
-    std::string concatenatedMessages;
-};
+
+
+
---
+++ server/server.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <string>
+#include <vector>
+#include <unordered_map>
+
+// int clientAddress - > clientID
+
+class Server {
+public:
+    Server();
+
+    void addNewClient(int clientId);
+    void removeClient(int clientId);
+
+    std::string requestMessageFromClient(int clientID);
+
+    void recieveMessageFromClient(int clientID, std::string message);
+
+    std::string concatenateAllMessages();   
+
+    void removeClient(int clientId);
+    
+    void broadcastResultsToAllClients(std::string results);
+
+private:
+    void changeClientMessage(int clientId, std::string newMessage);
+    std::vector<int> clientIDs; 
+    std::vector<std::string> clientMessages;
+    std::unordered_map<int, std::string> clientIDToMessageMap;
+    std::string concatenatedMessages;
+};
