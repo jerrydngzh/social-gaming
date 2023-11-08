dd973ca7081ee79b297243b16d51f70051b02700
jerrydngzh <zrd1@sfu.ca>
2023-11-07 20:32:56 -0800

    setup test drivers for GameRoom & Game

---
+++ test/testGame.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../server/Game/include/Game.h"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace testing;
+
+class GameTest : public::testing::Test {};
---
+++ test/testGameRoom.cpp
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "../server/GameRoom/include/GameRoom.h"
+#include <iostream>
+#include <string>
+#include <stdexcept>
+
+using namespace testing;
+
+class GameRoomTest : public::testing::Test {};
