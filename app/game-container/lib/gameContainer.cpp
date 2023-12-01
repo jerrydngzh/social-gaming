39c5680f7a031d541ec03e7c5e3beedada723ab0
cga57 <cga57@sfu.ca>
2023-11-30 21:55:27 -0800

    Adding the gameContainer join (accidental commit last time). Authors: cga57, jlc58

--- app/game-container/lib/gameContainer.cpp
+++ app/game-container/lib/gameContainer.cpp
@@ -4,6 +4,10 @@
 // Game Container Manager should pass in the server DTO right back in
 // we need a static pointer to game container manager as well
 
+void GameContianer::interpeterCommunication(){
+
+}
+
 GameContainer::GameContainer() : ownerID(0), gameInviteCode(0), playerList(), serverDTO(serverDTO){
     // The configuration we are expecting is the one included in this file (Our code)
     // but currently it returns the configuration in game-container (Mike's code)
@@ -16,9 +20,11 @@ GameContainer::GameContainer() : ownerID(0), gameInviteCode(0), playerList(), se
     InputOrJoin binaryEnum = JOIN;
     DTOtoGame dtoGame = {serverDTO.clientID,binaryEnum,clData};
     
+    interpeterCommunication();
 
     
 };
+
 
 
 
@@ -26,21 +32,28 @@ void GameContainer::getMsgFromGCManager(const C2SDTO& serverDTO)
 {
     if (serverDTO.command == "JOIN")
     {
+        
+        MemberState newPlayer{"temp", serverDTO.clientID};
         addPlayerToList(serverDTO.clientID);
+        game.addPlayer(&newPlayer);
+        
         
     }
     else if (serverDTO.command == "INPUT")
     {
         
-        // game->run();
-
     }
+}
+
+GameContainer::askForSetting() {
+    
+
 }
 
 DTOtoGameContainerManager GameContainer::sendMsgToGCManager()
 {
-    DTOtoGameContainerManager dto;
-    dto = {};
+    DTOtoGameContainerManager dto {};
+    // dto = {};
     // loop through each setting and send it one at a time, pause and rewait
     // to be discussed on Wed
     for (const auto& setting : settings) {
