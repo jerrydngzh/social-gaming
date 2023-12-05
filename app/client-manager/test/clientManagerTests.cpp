9d9857b2d8872c70640ebd60ec267bbee3b90926
jerrydngzh <jerrydeng67@gmail.com>
2023-12-04 19:28:30 -0800

    resolve build compile cmake error

---
+++ app/client-manager/test/clientManagerTests.cpp
@@ -0,0 +1,12 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+#include "ClientManager.h"
+
+
+// TODO: server manager tests require mocks for:
+//       server, game engine, message processor, and server processor
+TEST(ClientManagerTests, POC)
+{
+    EXPECT_TRUE(true);
+} // POC
