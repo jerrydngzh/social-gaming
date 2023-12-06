fa96c71de82bdc51dfb37370c6ae3d3a2be13dbe
jlc58 <jamie_chin_2@sfu.ca>
2023-12-06 15:13:21 -0800

    fixed tests and cleaned them up. Authors: jlc58

--- app/game-container/test/gameContainerTests.cpp
+++ app/game-container/test/gameContainerTests.cpp
@@ -2,95 +2,74 @@
 #include "gmock/gmock.h"
 #include "gameContainer.h"
 #include <tuple>
+#include <vector>
 
 using namespace testing;
 
 
 class GameContainerTests : public testing::Test {
 protected:
-    // const ClientsToServerDataObject joinCommandDTO = {1, "JOIN", "2", true};
-    std::variant<int, bool> myVariant = static_cast<int>(42);
-    std::tuple<int, int> myTuple = std::make_tuple(1, 2);
-    Setting::Kind kind = Setting::STRING;
-    Setting mySetting{"weapon", kind};
-    GameRequest request{"pause"} ;
+    int expectedClientId = 1;
+    Setting::Kind expectedKind = Setting::STRING;
+    Setting expectedSetting{"weapon", expectedKind};
+    bool expectedIsParallel = false;
+    std::variant<int, bool> expectedValue = static_cast<int>(42);
+    std::tuple<int, int> expectedRange = std::make_tuple(1, 2);
+    GameRequest expectedRequest{"command"};
 
-    // DTOtoGameContainerManager expectedJoinDTO = { 2, {}, mySetting, true, myVariant, myTuple, request};
+    std::vector<int> clientIds = { 3, 5, 0 }; 
+    ClientsToServerDataObject joinDTO = {1, "JOIN", "2", true};
+    GameContainer joinGameContainer{0, 0, clientIds, joinDTO};
+
+    ClientsToServerDataObject inputDTO = {1, "INPUT", "2", true};
+    GameContainer inputGameContainer{0, 0, clientIds, inputDTO};
+
+    GameContainer container{0, 0, clientIds, inputDTO};
 };
 
-TEST(GameContainerTests, POC) {
+TEST_F(GameContainerTests, POC) {
     EXPECT_TRUE(true);
 };
-// test is failing due to segfault with gamestate initialization (outside of gamecontainer)
-TEST(GameContainerTests, testConstructor){
-    // test gamefactory first
-    GameStateFactory gsFactory;
-    GameState game = gsFactory.createInitialGameState();
-    
-    ClientsToServerDataObject dto = {1, "JOIN", "2", true};
-    std::vector<int> clientsIds = {1,3,5,0};
-    GameContainer container{1, 1, clientsIds, dto};
-    
+
+TEST_F(GameContainerTests, testConstructor){
+    EXPECT_EQ(0, container.getOwnerID());
+    EXPECT_EQ(0, container.getGameInviteCode());
+    EXPECT_EQ(std::vector<int>({3, 5, 0}), container.getPlayerList());
 };
 
-TEST(GameContainerTests, testGameContainerManagerFunctionInputInvalid){
+TEST_F(GameContainerTests, testProcessCommandInput){ 
+    DTOtoGameContainerManager expectedInputDTO{expectedClientId, clientIds, expectedSetting,
+                                     expectedIsParallel, expectedValue, expectedRange, expectedRequest};
 
-    const ClientsToServerDataObject dto = {1, "INPUT", "2", true};
+    DTOtoGameContainerManager resultDTO = inputGameContainer.proccessCommandAndGetNextRequest(inputDTO);
 
-    std::vector<int> clientsIds = {1,3,5,0};
-    GameContainer container{1, 1, clientsIds, dto}; 
-
-
-    container.proccessCommandAndGetNextRequest(dto);
+    EXPECT_EQ(expectedInputDTO, resultDTO);
 };
 
-TEST(GameContainerTests, testProcessCommandJoin){
-    const ClientsToServerDataObject dto = {2, "JOIN", "2", true};
+TEST_F(GameContainerTests, testProcessCommandJoin) {
+    std::vector<int> expectedPlayerList = clientIds;
+    expectedPlayerList.push_back(1);  
 
-    std::vector<int> clientsIds = {1,3,5,0};
-    GameContainer container{1, 1, clientsIds, dto}; 
-    std::variant<int, bool> myVariant = static_cast<int>(42);
-    std::tuple<int, int> myTuple = std::make_tuple(1, 2);
-    Setting::Kind kind = Setting::STRING;
-    Setting mySetting{"weapon", kind};
-    // GameRequest request{"pause"} ;
+    DTOtoGameContainerManager expectedJoinDTO{expectedClientId, expectedPlayerList, expectedSetting,
+                                     expectedIsParallel, expectedValue, expectedRange, expectedRequest};
 
+    DTOtoGameContainerManager resultDTO = joinGameContainer.proccessCommandAndGetNextRequest(joinDTO);
 
-    DTOtoGameContainerManager resultDTO = container.proccessCommandAndGetNextRequest(dto);
-    DtoFromGame lastResponse = {false,1,"command",myVariant,myTuple,mySetting,{}};
-
-    GameRequest request{lastResponse.command};
-    DTOtoGameContainerManager expectedJoinDTO{lastResponse.clientID,{},lastResponse.setting,
-                                     lastResponse.isParallel,lastResponse.value,lastResponse.range, request};
-    // DTOtoGameContainerManager expectedJoinDTO = {2, {}, mySetting, true, myVariant, myTuple, request};
-
-    EXPECT_TRUE(expectedJoinDTO == resultDTO);
+    EXPECT_EQ(expectedJoinDTO, resultDTO);
 };
 
-TEST(GameContainerTests, testIsInValid) {
-    const ClientsToServerDataObject dto = {1, "JOIN", "2", true};
-
-    std::vector<int> clientsIds = {1,3,5,0};
-    GameContainer container{1, 1, clientsIds, dto}; 
+TEST_F(GameContainerTests, testIsInValid) {
     bool shouldBeFalse = container.validateInput("boo");
     EXPECT_FALSE(shouldBeFalse);
 };
 
-TEST(GameContainerTests, testIsValid) {
-    const ClientsToServerDataObject dto = {1, "JOIN", "2", true};
-
-    std::vector<int> clientsIds = {1,3,5,0};
-    GameContainer container{1, 1, clientsIds, dto}; 
+TEST_F(GameContainerTests, testIsValid) {
     container.addValidInput("foo");
     bool shouldBeTrue = container.validateInput("foo");
     EXPECT_TRUE(shouldBeTrue);
 };
 
-TEST(GameContainerTests, testIsValidandInvalid) {
-    const ClientsToServerDataObject dto = {1, "JOIN", "2", true};
-
-    std::vector<int> clientsIds = {1,3,5,0};
-    GameContainer container{1, 1, clientsIds, dto}; 
+TEST_F(GameContainerTests, testIsValidandInvalid) {
     container.addValidInput("foo");
     container.addValidInput("xoo");
     container.addValidInput("too");
@@ -99,22 +78,13 @@ TEST(GameContainerTests, testIsValidandInvalid) {
     bool shouldBeFalse = container.validateInput("hi");
     EXPECT_FALSE(shouldBeFalse);
 };
-TEST(GameContainerTests,testGameContainerProcesor){
-    const ClientsToServerDataObject dto = {1, "JOIN", "2", true};
-    std::vector<int> clientsIds = {1,3,5,0};
-    GameContainer container{1, 1, clientsIds, dto}; 
 
-    std::variant<int, bool> myVariant = static_cast<int>(42);
-    std::tuple<int, int> myTuple = std::make_tuple(1, 2);
-    Setting::Kind kind = Setting::STRING;
-    Setting mySetting("weapon", kind);
+TEST_F(GameContainerTests,testGameContainerProcesor){
+    DtoFromGame inputDto{expectedIsParallel,expectedClientId,"command",expectedValue,expectedRange,expectedSetting,{}};
+    DTOtoGameContainerManager dtoToManager = container.GameContainerProcessor(inputDto);
+    DTOtoGameContainerManager expectedOutput = {expectedClientId,clientIds,expectedSetting,expectedIsParallel,expectedValue,expectedRange,expectedRequest};
 
-    DtoFromGame myDto = {false,1,"command",myVariant,myTuple,mySetting,{}};
-    DTOtoGameContainerManager dtoToManager = container.GameContainerProcessor(myDto);
-    GameRequest request{"hello"};
-    DTOtoGameContainerManager expectedOutput = {1,{},mySetting,false,myVariant,myTuple};
-    EXPECT_TRUE(expectedOutput == dtoToManager);
-    // EXPECT_EQ(expectedOutput, dtoToManager);
+    EXPECT_EQ(expectedOutput, dtoToManager);
 }
 
 
